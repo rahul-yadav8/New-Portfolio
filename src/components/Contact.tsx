30bb9a7a1eedbc36c3182d92eddb716868f63ec1
Rahul Yadav <rahulyadav050597@gmail.com>
2026-02-02 14:14:20 +0530

    pushed new changes

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,53 +1,7 @@
-import React, { useRef, useState } from "react";
 import "../assets/styles/Contact.scss";
 // import emailjs from "@emailjs/browser";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import SendIcon from "@mui/icons-material/Send";
-import TextField from "@mui/material/TextField";
 
 function Contact() {
-  const [name, setName] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [message, setMessage] = useState<string>("");
-
-  const [nameError, setNameError] = useState<boolean>(false);
-  const [emailError, setEmailError] = useState<boolean>(false);
-  const [messageError, setMessageError] = useState<boolean>(false);
-
-  const form = useRef();
-
-  const sendEmail = (e: any) => {
-    e.preventDefault();
-
-    setNameError(name === "");
-    setEmailError(email === "");
-    setMessageError(message === "");
-
-    /* Uncomment below if you want to enable the emailJS */
-
-    // if (name !== "" && email !== "" && message !== "") {
-    //   var templateParams = {
-    //     name: name,
-    //     email: email,
-    //     message: message,
-    //   };
-
-    //   console.log(templateParams);
-    //   emailjs.send("service_id", "template_id", templateParams, "api_key").then(
-    //     (response) => {
-    //       console.log("SUCCESS!", response.status, response.text);
-    //     },
-    //     (error) => {
-    //       console.log("FAILED...", error);
-    //     }
-    //   );
-    //   setName("");
-    //   setEmail("");
-    //   setMessage("");
-    // }
-  };
-
   return (
     <div id="contact">
       <div className="items-container">
